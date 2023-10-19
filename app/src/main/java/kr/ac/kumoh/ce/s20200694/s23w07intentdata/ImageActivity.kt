2caeab2bc98232e87763415b8c312813602aa413
Yang <junsuk530@gmail.com>
2023-10-19 17:38:28 +0900

    ImageActivity생성

--- app/src/main/java/kr/ac/kumoh/ce/s20200694/s23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/s23w07intentdata/ImageActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.ce.s20200694.s23w07intentdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.ce.s20200694.s23w07intentdata.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
@@ -10,5 +11,12 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(main.root)
+        Log.i("Image!!!", intent.getStringExtra("soraka") ?: "없음")
+        val res = when(intent.getStringExtra(MainActivity.KEY_NAME)) {
+            "rem1" -> R.drawable.rem1
+            "rem2" -> R.drawable.rem2
+            else -> R.drawable.ic_launcher_foreground
+        }
+        main.image.setImageResource(res)
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20200694/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/s23w07intentdata/MainActivity.kt
@@ -1,18 +1,33 @@
 package kr.ac.kumoh.ce.s20200694.s23w07intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kr.ac.kumoh.ce.s20200694.s23w07intentdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_NAME = "soraka"
+    }
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
-        main.btn1.setOnClickListener { Toast.makeText(this, "리메이크 전", Toast.LENGTH_SHORT).show() }
-        main.btn2.setOnClickListener { Toast.makeText(this, "리메이크 후", Toast.LENGTH_SHORT).show() }
+        main.btn1.setOnClickListener {
+            Toast.makeText(this, "리메이크 전", Toast.LENGTH_SHORT).show()
+        var intent = Intent(this, ImageActivity::class.java)
+            intent.putExtra(KEY_NAME, "rem1")
+            startActivity(intent)
+        }
+        main.btn2.setOnClickListener {
+            Toast.makeText(this, "리메이크 후", Toast.LENGTH_SHORT).show()
+            var intent = Intent(this, ImageActivity::class.java)
+            intent.putExtra(KEY_NAME, "rem2")
+            startActivity(intent)
+
+        }
 
     }
 }
